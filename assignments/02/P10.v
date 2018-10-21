df420bb24e3880e451a3221b99cde18b28a0d1af
aqjune <aqjune@gmail.com>
2018-10-21 16:38:42 +0900

    Fix a bug in assignment 2 (P10.v, E01_01.v)

--- assignments/02/P10.v
+++ assignments/02/P10.v
@@ -3,6 +3,6 @@ Require Export P09.
 
 
 (** **** Problem #10 : 4 stars (tr_rev_correct) *)
-Lemma tr_rev_correct : forall X, @tr_rev X = @rev X.
+Lemma tr_rev_correct : forall X l, @tr_rev X l = @rev X l.
 Proof. exact FILL_IN_HERE. Qed.
 
